2858e318fdb9d60a026e8d357d2044b9674b1703
Victor Fernandes <90485435+VictorFMag@users.noreply.github.com>
2023-10-27 09:23:25 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.main;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author aluno
- */
 public class Main {
 
     public static void main(String[] args) {
